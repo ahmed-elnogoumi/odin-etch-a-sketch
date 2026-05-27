ba887ea0489429d47a6a35d82b8151293b6b99e0
ahmed-elnogoumi <ahmedelnogoumi@gmail.com>
2026-05-26 22:00:33 -0700

    Built project out per spec
    Completed project to spec minus extra credit -- all core functionality has been created

--- script.js
+++ script.js
@@ -1,15 +1,42 @@
 const squareContainer = document.getElementById(`squareContainer`);
+const resizeGridButton = document.getElementById(`resizeGridButton`);
 
+function createGrid(width, height) {
+    for (let i = 0; i < height; i++) {
+        let containerRow = document.createElement(`div`);
+        containerRow.className = `row`;
 
-// appending more divs into the squareContainer we just created
-for (let i = 0; i < 16; i++) {
-    let containerRow = document.createElement(`div`);
-    containerRow.id = `row${i}`;
-
-    for (let j = 0; j < 16; j++) {
-        let square = document.createElement(`div`);
-        square.className = `square`;
-        containerRow.append(square);
-    }
+        for (let j = 0; j < width; j++) {
+            let square = document.createElement(`div`);
+            square.style.width = `calc(500px / ${width})`;
+            square.style.height = `calc(500px / ${height})`;
+            square.height = `calc(500px / ${height})`;
+            square.className = `square`;
+            containerRow.append(square);
+            
+            square.addEventListener("mouseover", (event) => {
+            square.style.backgroundColor = `rgba(0, 0, 0, .1)`;
+            });
+        }
     squareContainer.append(containerRow);
+    }
 }
+
+function clearGrid(){
+    while(squareContainer.firstChild) {
+        squareContainer.removeChild(squareContainer.firstChild);
+    }
+}
+
+createGrid(16, 16);
+
+resizeGridButton.addEventListener("click", (event) => {
+    clearGrid();
+    let pixelSize = parseInt(prompt("Please enter the pixel size. (Size must be greater than 0 and equal or less than 100"));
+
+    while (pixelSize <= 0 || pixelSize > 100) {
+        pixelSize = parseInt(prompt("Please enter the pixel size. (Size must be greater than 0 and equal or less than 100"));
+    }
+    
+    createGrid(pixelSize, pixelSize);
+})
